88a5a84d7768378b87a4a139dcd1e8b05f5cf71a
Edgars Cīrulis <edgarsscirulis@gmail.com>
2024-01-01 22:46:45 +0200

    dm2q: Introduce crypto support
    Signed-off-by: Edgars Cīrulis <edgarsscirulis@gmail.com>

--- device.mk
+++ device.mk
@@ -21,3 +21,17 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+
+# qcom decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Dependencies
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libdmabufheap
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap.so
